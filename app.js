a51468e6db5d3d1af43a774f3e0a385eb45c7eeb
Vivian Dang <dangvv@dukes.jmu.edu>
2020-11-14 14:47:34 -0500

    connecting to database

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+const fs = require('fs');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
+let connection = mysql.createConnection(credentials);
+connection.connect();
+
+const port = 3443;
+app.listen(port, () => {
+    console.log(`We're live on port ${port}`);
+});
